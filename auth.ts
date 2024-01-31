f31b6ec5b081000f3f7d56b62de68b8b98ed6d4c
Sandeep Nijjar <sandeepnijjar@Sandeeps-MacBook-Pro.local>
2024-01-31 11:49:21 +0000

    add in trainerId

--- auth.ts
+++ auth.ts
@@ -8,5 +8,17 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.NEXT_GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async jwt({token, profile}) {
+      const trainerID = "TESTING_TRAINER_ID" // change
+      token.trainerId = trainerID
+      return token
+    },
+    async session({session, token}) {
+      session.user.trainerId = token.trainerId as string
+      return session
+    }
+
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,14 +1,28 @@
 "use client";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export const LoginForm = () => {
+  const { data } = useSession();
+
+  console.log({ data });
   return (
-    <button
-      onClick={() => {
-        signIn("google");
-      }}
-    >
-      Login
-    </button>
+    <div className="flex gap-6">
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          signOut();
+        }}
+      >
+        logout
+      </button>
+    </div>
   );
 };
